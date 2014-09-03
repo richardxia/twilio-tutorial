079240a28c648a56fd30cbf3b0b4115a7f2259bc
Richard Xia <rxia@counsyl.com>
2014-09-03 13:18:09 -0700

    Add get message details code.

--- example.py
+++ example.py
@@ -4,13 +4,26 @@ ACCOUNT_SID = None
 AUTH_TOKEN = None
 FROM_NUMBER = None
 
+# Send text message
 response = requests.post(
-    'https://{account_sid}:{auth_token}@api.twilio.com/2010-04-01/Accounts/{account_sid}/Messages'.format(
+    'https://{account_sid}:{auth_token}@api.twilio.com/2010-04-01/Accounts/{account_sid}/Messages.json'.format(
         account_sid=ACCOUNT_SID,
         auth_token=AUTH_TOKEN,
     ),
     data={'From': FROM_NUMBER,
-          'To': '+14083945058',
+          'To': 'PHONE_NUMBER_GOES_HERE',
           'Body': "Hello!"},
 )
 print response.status_code
+print response.content
+
+# Get message details
+#response = requests.get(
+#    'https://{account_sid}:{auth_token}@api.twilio.com/2010-04-01/Accounts/{account_sid}/Messages/{message_sid}.json'.format(
+#        account_sid=ACCOUNT_SID,
+#        auth_token=AUTH_TOKEN,
+#        message_sid='MESSAGE_SID_GOES_HERE',
+#    ),
+#)
+#print response.status_code
+#print response.content
